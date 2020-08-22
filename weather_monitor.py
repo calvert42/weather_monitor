ed6e2798657a4174072f6b01e352fd8e1a228534
rpotierferry <romainpotierferry@gmail.com>
2020-08-23 01:52:00 +0200

    refactored main module

--- weather_monitor.py
+++ weather_monitor.py
@@ -16,54 +16,53 @@ res_file = "data/result.json"
 units = "metric"
 api_key = os.environ.get('API_WEATHER')
 part = "minutely,daily"
-look_ahead = 2
+look_ahead = 5
 total = 1
 current_time = 0
 run_time = 0
 
 
-def init(city, res_file, units, api_key, part):
-    lat, lon = utils.get_location_coordinates(city)
-    owmap_request = utils.make_owmap_request(lat, lon, part, units, api_key)
-    weather = utils.send_request(owmap_request)
-    utils.save_json_file(weather, res_file)
-    weather = utils.open_json_file(res_file)
-    logger.info("Initialisation succeeded")
-    return weather
+class Weather_Monitor:
+    def __init__(self, weather=None):
+        lat, lon = utils.get_location_coordinates(city)
+        owmap_request = utils.make_owmap_request(
+            lat, lon, part, units, api_key)
+        weather = utils.send_request(owmap_request)
+        utils.save_json_file(weather, res_file)
+        self.weather = utils.open_json_file(res_file)
+        logger.info("Initialisation succeeded")
 
+    def main(self):
 
-def main():
+        wh = weather_handler.Weather_Handler(self.weather)
+        full_weather = wh.get_full_current_weather()
+        next_full_weather = wh.get_full_hourly_weather((look_ahead - 1))
+        current_weather = wh.get_weather_state(full_weather)
+        next_weather = wh.get_weather_state(next_full_weather)
+        weather_is_changing = wh.weather_is_changing(
+            current_weather, next_weather)
+        current_temp = wh.get_temperature(full_weather)
+        next_temp = wh.get_temperature(next_full_weather)
+        temp_is_changing = wh.temperature_is_changing(current_temp, next_temp)
 
-    weather = init(city, res_file, units, api_key, part)
+        """     message = ""
+        if weather_is_changing:
+            message += weather_is_changing + "\n"
 
-    wh = weather_handler.Weather_Handler(weather)
-    full_weather = wh.get_full_current_weather()
-    next_full_weather = wh.get_full_hourly_weather((look_ahead - 1))
-    current_weather = wh.get_weather_state(full_weather)
-    next_weather = wh.get_weather_state(next_full_weather)
-    weather_is_changing = wh.weather_is_changing(current_weather, next_weather)
-    current_temp = wh.get_temperature(full_weather)
-    next_temp = wh.get_temperature(next_full_weather)
-    temp_is_changing = wh.temperature_is_changing(current_temp, next_temp)
+        if temp_is_changing:
+            message += temp_is_changing + "\n"
 
+        if len(message) != 0:
+            message += "I'm a bot :)"
 
-"""     message = ""
-    if weather_is_changing:
-        message += weather_is_changing + "\n"
-
-    if temp_is_changing:
-        message += temp_is_changing + "\n"
-
-    if len(message) != 0:
-        message += "I'm a bot :)"
-
-        mb = mail_bot.Mail_Bot(USER, PASS)
-        mb.send_mail(receiver, message) """
+            mb = mail_bot.Mail_Bot(USER, PASS)
+            mb.send_mail(receiver, message) """
 
 
 if __name__ == "__main__":
     while current_time < total:
-        main()
+        wm = Weather_Monitor()
+        wm.main()
         current_time += 1
         logger.info(f"Process ran {current_time} time(s)")
         time.sleep(run_time)
